3724b5d9bce797bfc5d834ddad1257fb083ff635
Claudio <claudiocarchietti@yahoo.it>
2021-04-12 15:53:42 +0200

    v-for inserito nell'html con prova generale di film

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+Vue.config.devtools = true;
+
 var app = new Vue ({
   el: '#root',
+  data:{
+    movies:[],
+  },//fine data
+  mounted() {
+    axios.get("https://api.themoviedb.org/3/search/movie/?api_key=06aaa03812e7863dd3760e09e85808b3&query=''")
+    .then((response) => {
+      this.movies = response.data.results;
+      console.log(response.data.results );
+    })
+  }//fine mounted
 })// fine vue
+
+
+// https://api.themoviedb.org/3/search/movie/?api_key=06aaa03812e7863dd3760e09e85808b3&query=''
