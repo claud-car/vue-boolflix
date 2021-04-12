1722b1ec34a1b15ed02b79b33f7805b75ebbae1e
Claudio <claudiocarchietti@yahoo.it>
2021-04-12 17:29:29 +0200

    milestone 1 terminata

--- main.js
+++ main.js
@@ -11,17 +11,17 @@ var app = new Vue ({
   },//fine mounted
   methods:{
     searchMovie: function() {
-      axios.get(`https://api.themoviedb.org/3/search/movie/?api_key=06aaa03812e7863dd3760e09e85808b3`,
-      {params:
-        {
-          query: this.search
-        }
-      }
-    )
-      .then((response) => {
-        this.movies = response.data.results;
-        this.search = '';
-      });
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=06aaa03812e7863dd3760e09e85808b3&query=${this.search}`)
+        .then((response) => {
+          this.movies = response.data.results;
+          this.search = '';
+        });
+
+      // {params:
+      //   {
+      //     query: this.search
+      //   }
+      // }
     },
   }//fine methods
 })// fine vue
