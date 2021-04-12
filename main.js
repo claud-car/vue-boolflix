6c03bd5eaeb49210926bc61a0c3ddf1ac9369f3c
Claudio <claudiocarchietti@yahoo.it>
2021-04-12 16:44:07 +0200

    Milestone 1 terminata

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ Vue.config.devtools = true;
 var app = new Vue ({
   el: '#root',
   data:{
+    search:'',
     movies:[],
   },//fine data
   mounted() {
-    axios.get("https://api.themoviedb.org/3/search/movie/?api_key=06aaa03812e7863dd3760e09e85808b3&query=''")
-    .then((response) => {
-      this.movies = response.data.results;
-      console.log(response.data.results );
-    })
-  }//fine mounted
+
+  },//fine mounted
+  methods:{
+    searchMovie: function() {
+      axios.get(`https://api.themoviedb.org/3/search/movie/?api_key=06aaa03812e7863dd3760e09e85808b3`,
+      {params:
+        {
+          query: this.search
+        }
+      }
+    )
+      .then((response) => {
+        this.movies = response.data.results;
+        this.search = '';
+      });
+    },
+  }//fine methods
 })// fine vue
 
 
