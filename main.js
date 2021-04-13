735e2d7248dea42610984dae4236978d53863980
Claudio <claudiocarchietti@yahoo.it>
2021-04-13 15:21:03 +0200

    Milestone 3 completata

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue ({
   data:{
     search:'',
     movies:[],
+    tvSeries:[],
     link: 'https://api.themoviedb.org/3',
     apiKey:'06aaa03812e7863dd3760e09e85808b3',
     language: [],
@@ -12,20 +13,28 @@ var app = new Vue ({
   mounted() {
   },//fine mounted
   methods:{
+    searchAll() {
+      this.searchMovie();
+      this.searchTvSeries();
+      this.movies= '';
+      this.tvSeries= '';
+    },
     searchMovie: function() {
       axios.get(`${this.link}/search/movie?api_key=${this.apiKey}&query=${this.search}`)
         .then((response) => {
           this.movies = [...this.movies, ...response.data.results];
           this.search = '';
         });
-        axios.get(`${this.link}/search/tv?api_key=${this.apiKey}&query=${this.search}`)
-          .then((response) => {
-            this.movies = [...this.movies, ...response.data.results];
-            this.search = '';
-          });
-            this.movies= '';
-
-
+    },
+    searchTvSeries:function() {
+      axios.get(`${this.link}/search/tv?api_key=${this.apiKey}&query=${this.search}`)
+      .then((response) => {
+        this.tvSeries = [...this.tvSeries, ...response.data.results];
+        this.search = '';
+      });
+    },
+    stars: function(star) {
+      return Math.ceil( star.vote_average / 2 );
     },
   }//fine methods
 })// fine vue
